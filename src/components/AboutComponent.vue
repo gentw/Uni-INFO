23f5815c4b2fb03182be19882a1ed75b33200b54
gentw <gentiannuka@gmail.com>
2020-05-27 21:19:39 +0200

    minor changes

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -5,6 +5,74 @@
 
 export default {
 		name: 'about',
+		data() {
+			return {
+				aboutInfos: {
+					"cover_information": [],
+					"details": []
+
+				}
+				 
+				
+
+			}
+		},
+
+		methods: {
+
+			fetchAboutInfo: function() {
+				let ajaxRequests = [
+					{						
+						"global": [
+							{					
+								"limit": "all",
+								"type": "cover_information"
+							}
+						]
+					},
+					{						
+						"global": [
+							{					
+								"limit": "all",
+								"type": "details"
+							}
+						]
+					},
+					{						
+						"global": [
+							{					
+								"limit": "all",
+								"type": "team_heading_details"
+							}
+						]
+					},
+					{						
+						"global": [
+							{					
+								"limit": "all",
+								"type": "team_members"
+							}
+						]
+					}
+				];
+
+				this.$http.post('https://cms.uni-info.org/websites/api/global', ajaxRequests[0]).then((response) => {
+					
+					
+					this.aboutInfos.cover_information.push(response.body.global.one[0].cover_information.collections);
+					
+				});
+
+			},
+
+
+		},
+
+		mounted() {
+			this.fetchAboutInfo();
+
+			console.log(this.aboutInfos.cover_information);
+		}
 		
 
 } 
